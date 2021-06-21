7f5f87866eb3ecc1e860068e199f1c28cf88225e
Esizl <63180275+Esizl@users.noreply.github.com>
2021-06-21 04:16:46 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+// var countDownDate = new Date("Jun 22, 2021 6:00:00").getTime();
+var countDownDate = new Date("Jun 20, 2021 12:15:00").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get today's date and time
+  var now = new Date().getTime();
+    
+  // Find the distance between now and the count down date
+  var distance = countDownDate - now;
+    
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+  // Output the result in an element with id="demo"
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+    
+  // If the count down is over, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    setInterval(function() {
+      document.getElementById("timer").innerHTML = "TIMES UP <3";
+    }, 1000)
+  }
+}, 1000)
